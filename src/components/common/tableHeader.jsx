cc48251b47eb97c3146fd1cfab62b0f952401379
Yamendra Pun <punyamendra@gmail.com>
2021-10-31 01:10:58 +0900

    Sorting - Adding the Sort Icon

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -14,15 +14,23 @@ const TableHeader = (props) => {
     onSort(newSortColumn)
   }
 
+  function renderSortIcon(column) {
+    if (column.path !== sortColumn.path) return null
+    if (sortColumn.order === 'asc') return <i className='fa fa-sort-asc' />
+    return <i className='fa fa-sort-desc' />
+  }
+
   return (
     <thead>
       <tr>
         {columns.map((column) => (
           <th
+            className='clickable'
             key={column.path || column.key}
             onClick={() => raiseSort(column.path)}
           >
             {column.label}
+            {renderSortIcon(column)}
           </th>
         ))}
       </tr>
