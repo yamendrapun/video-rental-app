a19f995fd0625e8493f1f3032422b83e0e8bf7e3
Yamendra Pun <punyamendra@gmail.com>
2021-10-31 19:31:19 +0900

    handling multiple inputs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginForm = () => {
+  const [account, setAccount] = useState({ username: '', password: '' })
+
   function handleSubmit(e) {
     e.preventDefault()
 
     // Call the server
     console.log('Submitted')
+  }
+
+  function handleChange({ currentTarget: input }) {
+    let newAccount = { ...account }
+    newAccount[input.name] = input.value
+    setAccount(newAccount)
   }
 
   return (
@@ -14,11 +22,25 @@ const LoginForm = () => {
       <form onSubmit={handleSubmit}>
         <div className='mb-3'>
           <label htmlFor='username'>Username</label>
-          <input id='username' type='text' className='form-control' />
+          <input
+            value={account.username}
+            onChange={handleChange}
+            id='username'
+            name='username'
+            type='text'
+            className='form-control'
+          />
         </div>
         <div className='mb-3'>
           <label htmlFor='password'>Password</label>
-          <input id='password' type='text' className='form-control' />
+          <input
+            value={account.password}
+            onChange={handleChange}
+            id='password'
+            name='password'
+            type='text'
+            className='form-control'
+          />
         </div>
         <button className='btn btn-primary'>Login</button>
       </form>
